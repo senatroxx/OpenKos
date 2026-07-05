ca3f38461bd132edc228f01907ee24a83465e45d
Muhammad Athhar Kautsar <athharkautsar14@gmail.com>
2026-07-05 16:41:15 +0700

    fix: align notification template with :unit, docs field names, and component exports

--- resources/js/components/features/units/index.ts
+++ resources/js/components/features/units/index.ts
@@ -1,3 +1,3 @@
-export { default as RoomDetailSheet } from './unit-detail-sheet';
-export { default as RoomFormSheet } from './unit-form-sheet';
-export { default as RoomOverview } from './unit-overview';
+export { default as UnitDetailSheet } from './unit-detail-sheet';
+export { default as UnitFormSheet } from './unit-form-sheet';
+export { default as UnitOverview } from './unit-overview';
