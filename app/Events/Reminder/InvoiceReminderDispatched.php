d67c9c5f69ce02a0827e79d5ef676830aa6cf2af
Muhammad Athhar Kautsar <athharkautsar14@gmail.com>
2026-07-12 03:27:13 +0700

    fix: drop Invoice from event, iterate events in ForceSendReminder

--- app/Events/Reminder/InvoiceReminderDispatched.php
+++ app/Events/Reminder/InvoiceReminderDispatched.php
@@ -3,7 +3,6 @@
 namespace App\Events\Reminder;
 
 use App\Data\Reminder\ReminderEvent;
-use App\Models\Invoice;
 use Illuminate\Foundation\Events\Dispatchable;
 
 class InvoiceReminderDispatched
@@ -12,6 +11,5 @@ class InvoiceReminderDispatched
 
     public function __construct(
         public readonly ReminderEvent $event,
-        public readonly ?Invoice $invoice = null,
     ) {}
 }
